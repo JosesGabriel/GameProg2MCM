7ea3a39ff571243d161af0a55ad56bf579980e41
Denise Suarez <suarez.dma@gmail.com>
2020-09-16 11:08:38 +0800

    week_08 initial commit

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+
+            //Player player = other.GetComponent<Player>();
+            //if (player != null) player.Die();
+            SceneManager.LoadScene("Main");
+
+            Debug.Log("test :>");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
